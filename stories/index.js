b5e5d1597f3b4171056dd49c880c60a9855f8265
sarasabet <sara.ilbeigi@gmail.com>
2021-09-29 20:10:50 -0400

    add Error componenet/story

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,8 @@ import Empty from "components/Appointment /Empty.js";
 import Show from "components/Appointment /Show.js";
 import Confirm from "components/Appointment /Confirm";
 import Status from "components/Appointment /Status";
+import Error from "components/Appointment /Error";
+
 
 
 
@@ -147,6 +149,7 @@ storiesOf("Appointment", module)
   .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")}/>)
   .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")}/>)
   .add("Status", () => <Status message="Deleting" />)
+  .add ("Error", () => <Error message="Could not delete appointment."/>)
 
 
 
